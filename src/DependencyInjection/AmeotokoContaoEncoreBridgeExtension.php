aa76cd37db7c969830fe42f2e29093c23d41110b
ameotoko <andrey.vinichenko@gmail.com>
2022-07-04 16:54:42 +0200

    update extension class name

--- src/DependencyInjection/AmeotokoContaoEncoreBridgeExtension.php
+++ src/DependencyInjection/AmeotokoContaoEncoreBridgeExtension.php
@@ -11,7 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class ContaoEncoreBridgeExtension extends Extension
+class AmeotokoContaoEncoreBridgeExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
